7201f40af6954b80df2c15e8f777f061b62d6520
dewslyse <dewslyse@gmail.com>
2022-07-06 22:27:34 +0000

    Add method for updating posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :user, counter_cache: :posts_counter
+  belongs_to :user
   has_many :comments
   has_many :likes
 
+  after_save :update_posts_counter
+
   def recent_comments
     comments.limit(5).order(created_at: :desc)
+  end
+
+  private
+
+  def update_posts_counter
+    user.increment!(:posts_counter)
   end
 end
