15bc9c4314ffb2b4dcb6021a406c5876b75cb48d
dewslyse <dewslyse@gmail.com>
2022-07-06 20:06:12 +0000

    Return the 5 most recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user, counter_cache: :posts_counter
   has_many :comments
   has_many :likes
+
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 end
