873b598d7cee6483e8af282c9b8f2f83422b0873
dewslyse <dewslyse@gmail.com>
2022-07-14 19:57:09 +0000

    Add new action to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
   def create
     @user = current_user
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(user: @user, post: @post, text: params[:text])
+    @comment = @user.comments.new(post: @post, text: params[:text])
     @comment.save
     redirect_to user_post_path(@user, @post)
   end
