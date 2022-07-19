454834d4fd9f4ef3235315ef4f8b9c74e3491181
dewslyse <dewslyse@gmail.com>
2022-07-19 07:02:08 +0000

    Fix N+1 issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,6 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true
+    Bullet.raise = true # raise an error if an n+1 query occurs
   end
 end
