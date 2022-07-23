6591d9b1e7f26f69ab7146d2f784e9c3841de7d4
dewslyse <dewslyse@gmail.com>
2022-07-23 01:18:56 +0000

    Fix linter errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,19 +1,13 @@
 class Api::V1::CommentsController < ApplicationController
   def index
-<<<<<<< HEAD
     @post = Post.find(params[:post_id])
     @comments = @post.comments.includes(:author)
     render json: @comments, status: :ok
-=======
-    @comments = Comment.all
-    render json: @comments, only: [:id, :text]
->>>>>>> 754214ce719e477f5933206b3414f8d314b90e98
   end
 
   def create
     @user = current_user
     @post = Post.find(params[:post_id])
-<<<<<<< HEAD
     @comment = @user.comments.new(post: @post, text: params[:text])
 
     if @comment.save
@@ -22,19 +16,4 @@ class Api::V1::CommentsController < ApplicationController
       render json: @comment.errors, status: :bad_request, message: 'Operation failed'
     end
   end
-=======
-    @comment = @user.comments.new(@user, @post, comment_params)
-    if @comment.save
-      render json: @comment
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:text)
-  end
->>>>>>> 754214ce719e477f5933206b3414f8d314b90e98
 end
